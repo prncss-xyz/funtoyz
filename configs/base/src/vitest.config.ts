fd7d6e3e6493d5764de283db7af9df903844d6b7
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-09-14 17:34:00 -0400

    chore: unified vitest config

---
+++ configs/base/src/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+	test: {
+		coverage: {
+			exclude: ['**.test.*'],
+			include: ['src/**/*.{js,ts,jsx,tsx}'],
+			provider: 'v8',
+			reporter: ['text', 'json'],
+			thresholds: {
+				branches: 100,
+				functions: 100,
+				lines: 100,
+				statements: 100,
+			},
+		},
+		globals: true,
+		include: ['src/**/*.test.{js,ts,jsx,tsx}'],
+	},
+})
