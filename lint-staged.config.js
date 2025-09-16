e704748c0b9223057c89100aabcaef1e2ff21b70
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-09-16 00:54:51 -0400

    chore: improve lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
+	'!*.{js,jsx,ts,tsx}': ['prettier --write'],
 	'*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
-	'*.{json,md,yaml,yml}': ['prettier --write'],
 }
