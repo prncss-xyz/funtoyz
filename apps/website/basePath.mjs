acc65d6c199950b64b41261fe2d8e435aae2e4d2
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-09-16 20:59:30 -0400

    chore: fix ci

--- apps/website/basePath.mjs
+++ apps/website/basePath.mjs
@@ -1,10 +1,1 @@
-function getBasePath() {
-	// eslint-disable-next-line no-undef
-	const repo = process.env.GITHUB_REPOSITORY
-	if (!repo) return undefined
-	const [, name] = repo.split('/')
-	if (!name) return undefined
-	return '/' + name
-}
-
-export const basePath = getBasePath()
+export const basePath = '/funtoyz'
