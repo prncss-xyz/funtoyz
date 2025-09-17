be0859f78ef2e10cb93640aa642dc7d63a6d0ee6
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-09-16 20:00:25 -0400

    ci: fix build dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -42,6 +42,7 @@ export default defineConfig(
 				{
 					cspell: {
 						words: [
+							'logomark',
 							'accs',
 							'LTAGS',
 							'callbags',
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,10 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-	'*.{js,jsx,ts,tsx,cjs,mjs,mts,cts}': ['prettier --write', 'eslint --fix'],
+	'*.{js,jsx,ts,tsx,cjs,mjs,mts,cts}': [
+		'prettier --write',
+		'eslint --fix  --max-warnings 0',
+	],
 	'*.{json,jsonc,json5,md,mdx,markdown,yaml,yml,css,less,scss,html,gql,graphql,hbs,handlebars,vue}':
 		'prettier --write',
 }
