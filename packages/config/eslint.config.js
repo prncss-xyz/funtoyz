06ab5bbb53101378dcd21aaa64ec10ef3d3e1a25
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2025-09-21 19:26:08 -0400

    Revert "chore: multiple eslint configs"
    This reverts commit 2a9a0acf18c5263a66632a932a51a985b87cbbbd.

--- packages/config/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { sharedESLint } from './src/eslint.js'
-
-/** @type {import("eslint").Linter.Config} */
-export default sharedESLint
--- packages/core/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { sharedESLint } from '@funtoyz/config'
-
-/** @type {import("eslint").Linter.Config} */
-export default sharedESLint
