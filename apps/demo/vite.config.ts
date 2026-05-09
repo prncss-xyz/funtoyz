191bd78de80db5c42b292f92cf5bcc78308ccdd6
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-05-08 21:10:13 -0400

    ci: fix

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,13 +1,8 @@
-import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite-plus'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [['babel-plugin-react-compiler']],
-			},
-		}),
-	],
+	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 })
